7f8604e6b97fd4dc310236772b74d7d1abdc969a
HapaxHypatia <r.haskmann@gmail.com>
2025-02-21 13:36:22 +1000

    Completed add button, styled add popup

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-// TODO Add task button
 // TODO Edit task button
 // TODO save changes back to jsonfile
 // TODO Recurring tasks
 // TODO Date
 // TODO Categories
 // TODO sort and filter
+
+// Problems
+// TODO task list with few notes displays with big vertical gap because of min height
 
 
 function allowDrop(ev) {
@@ -61,42 +63,98 @@ function load (url) {
   })
 }
 
+function updateDB(taskdata){
+    let task = {
+      "ID": taskdata.ID,
+      "Title": taskdata.Title,
+      "Description/Notes": "",
+      "Type": "contact",
+      "Type__1": "",
+      "Category": "home",
+      "Deadline": "FALSE",
+      "Time estimate": "",
+      "Recurring?": "FALSE",
+      "Schedule": "",
+      "Subtasks": "",
+      "Prerequisites": "",
+      "Status": taskdata.Status,
+      "Resources": ""
+    }
+    load('./sampleTasks.json').then((jsonfile) => jsonfile.tasks)
+    .then((tasks) => {
+        tasks.push(task)
+        // TODO returns success code but doesn't alter tasks file
+        let promise = new Promise(async function (resolve, reject) {
+        const res = await fetch(
+          './sampleTasks.json', {
+          method: 'POST',
+          body: tasks
+        }
+        )
+        resolve(console.log(res))
+        }).then((result) => {
+            console.log('Success:', result);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+      }
+    )
+}
 
-function createTask(container, taskdata){
-  // Save new task to json
-  let parent = document.getElementById(container)
+
+function createTask(containerID, taskdata){
+  //Create the task div
   let div = document.createElement('div')
-  div.classList.add('note')
-  div.classList.add(taskdata.Status)
   div.id = taskdata.ID
   div.setAttribute("draggable", "true")
+  div.classList.add('note')
+
+  div.classList.add(taskdata.Status)
   div.innerText = taskdata.Title
+
+  //Place in container
+  let parent = document.getElementById(containerID)
   parent.appendChild(div)
+
   // Get the newly created div from the DOM & assign handler
   let newdiv = document.getElementById(taskdata.ID)
   newdiv.addEventListener("dragstart", drag)
 }
 
-function openPopup(){
+function openPopup(id){
   // Pop up window to get new task data.
-  let popup = document.getElementById("add-task-popup")
+  let popup = document.getElementById(id)
   popup.style.visibility = "visible"
+}
+
+function closePopup(id){
+  let popup = document.getElementById(id)
+  popup.style.visibility = "hidden"
 }
 
 function addTask(e){
   e.preventDefault()
   const data = Object.fromEntries(new FormData(e.target).entries());
   console.log(data)
+  //get ids of all task divs
+  const ids = [...document.getElementsByClassName('note')]
+  .map(element => Number(element.id));
+  //find highest id, add 1
+  const taskID = Math.max( ...ids )+1
+  data.ID = taskID
+  updateDB(data)
+  // Create task using data
+  let containerID = data.Status + "-list"
+  createTask(containerID, data)
+  closePopup('add-task-popup')
 }
 
-function editTask(){}
-
-function closePopup(e){
-  e.preventDefault()
-  let popup = e.target.parentElement
-  popup.style.visibility = "hidden"
+function editTask(e){
 
 }
+
+
 
 // Everything processed in then statement so that all functions have access to tasks
 load('./sampleTasks.json').then((jsonfile) => jsonfile.tasks).then((tasks) => {
